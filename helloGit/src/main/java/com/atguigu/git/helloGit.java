2041f43b3a6a0d875ab70ad293e9ce0d33f6b5c2
pipisay <1095583088@qq.com>
2021-03-13 22:42:41 +0800

    测试SSH

--- helloGit/src/main/java/com/atguigu/git/helloGit.java
+++ helloGit/src/main/java/com/atguigu/git/helloGit.java
@@ -10,5 +10,6 @@ public class helloGit {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("在线编辑");
+        System.out.println("测试SSH");
     }
 }
