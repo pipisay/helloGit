4fc8e6d5edaf38c37f954eeb2beb71d87bcbb532
王说 <1095583088@qq.com>
2021-04-06 22:16:18 +0800

    测试dev_p1

--- helloGit/src/main/java/com/atguigu/git/helloGit.java
+++ helloGit/src/main/java/com/atguigu/git/helloGit.java
@@ -7,6 +7,6 @@ public class helloGit {
         System.out.println("更新");
         System.out.println("更新2");
         System.out.println("创建分支");
-        System.out.println("分支添加123");
+        System.out.println("分支添加12345");
     }
 }
